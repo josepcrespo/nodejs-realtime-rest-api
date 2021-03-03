9965aa9008fa1265c93f6a1db1cf010a8eb076ed
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2021-03-03 07:22:05 +0000

    Bugfix: Finally, the bug that's causing all errors on tests.

--- src/services/sales/sales.class.js
+++ src/services/sales/sales.class.js
@@ -3,9 +3,9 @@ const { Service } = require('feathers-sequelize');
 exports.Sales = class Sales extends Service {
   create (data, params) {
     // This is the information we want from the sale signup data
-    const { model, engine, doors, color, extras } = data;
+    const { model, engine, doors, color, extras, createdById } = data;
     // The complete sale
-    const saleData = { model, engine, doors, color, extras };
+    const saleData = { model, engine, doors, color, extras, createdById };
 
     // Call the original `create` method with existing `params` and new data
     return super.create(saleData, params);
